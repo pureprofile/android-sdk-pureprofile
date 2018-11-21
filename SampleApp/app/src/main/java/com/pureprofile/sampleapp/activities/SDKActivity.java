62bef56ceee185ff451353fc904d357bf7559d45
George Sylaios <george.sylaios@gmail.com>
2018-11-21 09:23:00 +0200

    added sdk payment

--- SampleApp/app/src/main/java/com/pureprofile/sampleapp/activities/SDKActivity.java
+++ SampleApp/app/src/main/java/com/pureprofile/sampleapp/activities/SDKActivity.java
@@ -1,18 +1,22 @@
 package com.pureprofile.sampleapp.activities;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.pureprofile.sampleapp.R;
 import com.pureprofile.sdk.SdkApp;
 import com.pureprofile.sampleapp.model.Token;
 import com.pureprofile.sdk.ui.helpers.SdkActivity;
+import com.pureprofile.sdk.ui.listeners.PaymentListener;
 
-public class SDKActivity extends SdkActivity {
+public class SDKActivity extends SdkActivity implements PaymentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mInstance = SdkApp.init(this, Token.getToken(this));
+        mInstance.registerPaymentListener(this);
     }
 
     @Override
@@ -20,5 +24,13 @@ public class SDKActivity extends SdkActivity {
         super.onDestroy();
 
         mInstance.destroy();
+    }
+
+    @Override
+    public void onProcessPayment(String date, String key, Float payment) {
+//        Handle payments received from Pureprofile paid surveys.
+        Toast.makeText(this, String.format(getResources().getString(
+                R.string.payment_info), key, payment, date), Toast.LENGTH_SHORT)
+                .show();
     }
 }
