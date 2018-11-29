b032d4cf439475e6fa4dfe13bce64fa994166d5a
George Sylaios <george.sylaios@gmail.com>
2018-11-29 13:39:34 +0200

    remove auth service from sample app

--- SampleApp/app/src/main/java/com/pureprofile/sampleapp/services/AhService.java
+++
@@ -1,8 +0,0 @@
-package com.pureprofile.sampleapp.services;
-
-public class AhService {
-
-    public static final String SERVICE_ENDPOINT = "https://staging-ah-api.pureprofile.com/api/v1/";
-
-    public static final String INSTANCE = "public/instance/%1$s/info";
-}
