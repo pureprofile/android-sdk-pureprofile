4c1e2e37ea64ceab3e49ab8ac9a0957d2cf43b68
George Sylaios <george.sylaios@gmail.com>
2019-05-15 15:37:43 +0300

    change sdk version, remove extending of SdkActivity

--- SampleApp/app/src/main/java/com/pureprofile/sampleapp/activities/SDKActivity.java
+++ SampleApp/app/src/main/java/com/pureprofile/sampleapp/activities/SDKActivity.java
@@ -1,16 +1,16 @@
 package com.pureprofile.sampleapp.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.pureprofile.sampleapp.R;
 import com.pureprofile.sdk.SdkApp;
 import com.pureprofile.sampleapp.model.Token;
 import com.pureprofile.sdk.events.PaymentEvent;
-import com.pureprofile.sdk.ui.helpers.SdkActivity;
 import com.pureprofile.sdk.ui.listeners.PaymentListener;
 
-public class SDKActivity extends SdkActivity implements PaymentListener {
+public class SDKActivity extends AppCompatActivity implements PaymentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,8 @@ public class SDKActivity extends SdkActivity implements PaymentListener {
         SdkApp.getInstance().init(this, Token.getToken(this));
         SdkApp.getInstance().registerPaymentListener(this);
         SdkApp.getInstance().setTestEnv(this, false);
-        SdkApp.getInstance().run(this);
+        SdkApp.getInstance().hasNavigation(false);
+        SdkApp.getInstance().run(this, true);
     }
 
     @Override
