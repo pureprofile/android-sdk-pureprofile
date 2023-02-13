67aef70199bc1ed13e43af16952135eb0ea18355
gsylaios <gsylaios@pureprofile.com>
2023-02-13 11:40:05 +0200

    Updates panel props to dev

--- SampleApp/app/src/main/java/com/pureprofile/sampleapp/services/AuthService.java
+++ SampleApp/app/src/main/java/com/pureprofile/sampleapp/services/AuthService.java
@@ -2,5 +2,5 @@ package com.pureprofile.sampleapp.services;
 
 public class AuthService {
 
-    public static final String LOGIN_SERVICE = "https://pp-auth-api.pureprofile.com/api/v1/panel/login";
+    public static final String LOGIN_SERVICE = "https://dev-auth-api.pureprofile.com/api/v1/panel/login";
 }
